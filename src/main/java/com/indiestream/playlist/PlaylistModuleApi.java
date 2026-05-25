ca09b5d8beca21fd9969fe4238b80e269cf02b78
Oleksi89 <derdef360@gmail.com>
2026-05-25 19:38:37 +0300

    feat(playlist): expose internal projection APIs for lightweight library compilation

---
+++ src/main/java/com/indiestream/playlist/PlaylistModuleApi.java
@@ -0,0 +1,10 @@
+package com.indiestream.playlist;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface PlaylistModuleApi {
+    List<PlaylistLibraryProjection> getOwnedPlaylistsForLibrary(UUID userId);
+
+    List<PlaylistLibraryProjection> getFollowedPlaylistsForLibrary(UUID userId);
+}
